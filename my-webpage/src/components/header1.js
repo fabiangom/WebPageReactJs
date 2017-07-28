81b2d74e3e64f8318a7db27071529f413122961f
Fabián Gómez <fabianjgc95@gmail.com>
2017-07-28 15:39:36 -0500

    Add new changes in header1.js change a list of ancors using .map

--- my-webpage/src/components/header1.js
+++ my-webpage/src/components/header1.js
@@ -3,6 +3,13 @@ import React from 'react';
 // import '../index.css';
 import '../font-awesome-4.7.0/css/font-awesome.css';
 
+var icons = [ <i className="fa fa-facebook" aria-hidden="true"></i>,
+              <i className="fa fa-twitter" aria-hidden="true"></i>,
+              <i className="fa fa-google-plus" aria-hidden="true"></i>,
+              <i className="fa fa-youtube-play" aria-hidden="true"></i>,
+              <i className="fa fa-rss" aria-hidden="true"></i>
+            ];
+
 export default class HeaderOne extends React.Component {
   render(){
     return (
@@ -13,11 +20,16 @@ export default class HeaderOne extends React.Component {
             <p>Mi perfil es</p>
           </div>
           <div className="top-row-2">
-            <a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a>
+            {icons.map((current, ind) => {
+              return (
+                <a href="#">{current}</a>
+              )
+            })}
+            {/* <a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a>
             <a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a>
             <a href="#"><i className="fa fa-google-plus" aria-hidden="true"></i></a>
             <a href="#"><i className="fa fa-youtube-play" aria-hidden="true"></i></a>
-            <a href="#"><i className="fa fa-rss" aria-hidden="true"></i></a>
+            <a href="#"><i className="fa fa-rss" aria-hidden="true"></i></a> */}
           </div>
         </section>
       </header>
